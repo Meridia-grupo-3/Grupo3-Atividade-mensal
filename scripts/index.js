1133b190d3a74f6d0c1eb3593d32b8419611982e
Roberto Passos <rvpassos@hotmail.com>
2022-12-06 23:26:58 -0300

    Função carregar mais

--- scripts/index.js
+++ scripts/index.js
@@ -8,16 +8,30 @@ const options = {
 
 fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
 	.then(response => response.json())
-	.then ( jsonObject => {
-        innerBanner = document.querySelector('.page-banner');
-        innerGames = document.querySelector('.games-wrapper');
-        innerBanner.innerHTML = `<img src="${jsonObject[0].thumbnail}" alt="">`;
-        
-        for (let i = 1; i < 7; i++) {
-            innerGames.innerHTML += `<img src="${jsonObject[i].thumbnail}" alt="";>`;
-        }
-
-    })
-    
+	.then (jsonObject => ColocarDados(jsonObject))
     .then(response => console.log(response))
-	.catch(err => console.error(err));
+    .catch(err => console.error(err));
+
+
+
+function ColocarDados(jsonObject){
+    let innerBanner = document.querySelector('.page-banner');
+    let innerGames = document.querySelector('.games-wrapper');
+    innerBanner.innerHTML = `<img src="${jsonObject[0].thumbnail}" alt="">`;
+    
+    for (let i = 1; i < 7; i++) {
+        innerGames.innerHTML += `<img src="${jsonObject[i].thumbnail}" alt="";>`;
+    }
+
+    let contador = 13;
+    let i = 7;
+    let btn = document.getElementById('btn');
+
+    function elementos(){
+        for(i; i<contador;i++){
+            innerGames.innerHTML += `<img src="${jsonObject[i].thumbnail}" alt="">`
+        }
+        contador += 6;
+    }
+    btn.addEventListener('click', elementos);
+}   
