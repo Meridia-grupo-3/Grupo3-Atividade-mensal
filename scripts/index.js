ae7802b626019cc3638fb7247175f1504bafb27f
xtokram <xtokram@gmail.com>
2022-12-10 18:51:53 -0300

    corrigido bug do filter

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
+let innerBanner = document.querySelector('.video-banner');
+let innerGameBanner = document.querySelector('.game-banner');
+let innerGames = document.querySelector('.grid');
+var jogos_exibidos; 
+var limitador = 13;
+var contadorBusca = 7;
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -7,21 +14,18 @@ const options = {
 };
 
 
-fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
+	/*fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
 	.then(response => response.json())
 	.then (jsonObject => ColocarDados(jsonObject))
 	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.catch(err => console.error(err));*/
 
+	
 
-
-function ColocarDados(jsonObject){
-	let innerBanner = document.querySelector('.video-banner');
-	let innerGameBanner = document.querySelector('.game-banner');
-	let innerGames = document.querySelector('.grid');
-
-    innerBanner.innerHTML = `<video autoplay="true" loop="true"><source src="https://www.freetogame.com/g/${jsonObject[0].id}/videoplayback.webm"><source><video>`;
-								
+function ColocarDados(){
+	
+    var jsonObject = jogos_exibidos;
+    innerBanner.innerHTML = `<video autoplay="true" loop="true"><source src="https://www.freetogame.com/g/${jsonObject[0].id}/videoplayback.webm"><source><video>`;						
     innerGameBanner.innerHTML = `<div class="game-description">
 									<h1>${jsonObject[0].title}</h1>
 									<p>${jsonObject[0].platform}</p>
@@ -43,36 +47,37 @@ function ColocarDados(jsonObject){
 											<p>${jsonObject[i].short_description}</p>
 										</div>    
 									</article>`;
-	}
-
-	let contador = 13;
-	let i = 7;
+	}	
+	limitador = 13;
+	contadorBusca = 7;
 	let btn = document.getElementById('btn');
-
-	function elementos(){
-		for(i; i<contador;i++){
-			innerGames.innerHTML += `<article class="card-games">
-									<div class="fav-game">
-										<i class="fa-regular fa-star"></i>
-										<a href="${jsonObject[i].game_url}" target= "_blank">
-											<img src="${jsonObject[i].thumbnail}" alt="">
-									 	</a>
-									</div>
-									<div class="game-description">
-										<a href="${jsonObject[i].game_url}" target= "_blank">${jsonObject[i].title}</a>
-										<p>${jsonObject[i].platform}</p>
-										<p>${jsonObject[i].short_description}</p>
-									</div>    
-								</article>`
-		}
-		contador += 6;
-	}
 	btn.addEventListener('click', elementos);
-	
 }   
 
+function elementos(){
+	let jsonObject = jogos_exibidos;
+	for(contadorBusca; contadorBusca < limitador; contadorBusca++){	
+		if(contadorBusca >= jsonObject.length)
+			return;
+		innerGames.innerHTML += `<article class="card-games">
+								<div class="fav-game">
+									<i class="fa-regular fa-star"></i>
+									<a href="${jsonObject[contadorBusca].game_url}" target= "_blank">
+										<img src="${jsonObject[contadorBusca].thumbnail}" alt="">
+									 </a>
+								</div>
+								<div class="game-description">
+									<a href="${jsonObject[contadorBusca].game_url }" target= "_blank">${jsonObject[contadorBusca].title}</a>
+									<p>${jsonObject[contadorBusca].platform}</p>
+									<p>${jsonObject[contadorBusca].short_description}</p>
+								</div>    
+							</article>`
+	}
+	contadorBusca = contadorBusca + 6;
+	limitador = contadorBusca + 6;
+}
 
-let url_string = "https://www.freetogame.com/api/games"
+
 
 
 //Variáveis filtros
@@ -82,9 +87,14 @@ let filter_category = ""
 
 
 function loadPage(){
+	innerGames.innerHTML = ''; 
     fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?${filter_category}${filter_platform}`, options)
 	.then(response => response.json())
-    .then (jsonObject => ColocarDados(jsonObject)) 
+    .then (jsonObject => {
+		jogos_exibidos = jsonObject;
+		console.log(jogos_exibidos)
+		ColocarDados();
+	}) 
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 }
@@ -92,16 +102,22 @@ function loadPage(){
 function filterHome(){
     filter_platform = ""
     filter_category = ""
+	innerGames.innerHTML = ''; 
 
     fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?$?sort-by=popularity`, options)
 	.then(response => response.json())
-    .then (jsonObject => ColocarDados(jsonObject)) 
+    .then (jsonObject => {
+		jogos_exibidos = jsonObject;
+		console.log(jogos_exibidos);
+		ColocarDados();
+	}) 
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
 }
 
-function filterPlatform(plataform){             
+function filterPlatform(plataform){   
+	innerGames.innerHTML = '';          
     if(filter_category.length > 2){                 //caso haja um filtro de categoria, será colocado um & antes do parametro.
         filter_platform = `&platform=${plataform}`;
     }
@@ -113,6 +129,9 @@ function filterPlatform(plataform){
 }
 
 function filterCategory(category){
-    filter_category = `tag=${category}`;
+	innerGames.innerHTML = ''; 
+    filter_category = `category=${category}`;
     loadPage()
 }
+
+filterHome();
